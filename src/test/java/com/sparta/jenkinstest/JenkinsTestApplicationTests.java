4abdace6bb6a403ff1568f42538147f6c95ae997
jh.kim <start4321@hanmail.net>
2024-04-14 02:10:17 +0900

    feat: test commit

---
+++ src/test/java/com/sparta/jenkinstest/JenkinsTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.jenkinstest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
